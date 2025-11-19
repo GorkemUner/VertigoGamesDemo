c099f4aac672cfa570bc6b3bd139b2a7807e8e57
GorkemUner <gorkemuner00@gmail.com>
2025-11-19 09:57:25 +0300

    Refactor script folder structure for organization
    Moved scripts into categorized subfolders (Controllers, CustomEditor, Data, Managers, Panel) to improve project organization. Also updated InfiniteZoneSlider to increase shiftTime and removed commented and redundant code for clarity.

--- Assets/Scripts/Panel/InfiniteZoneSlider.cs
+++ Assets/Scripts/Panel/InfiniteZoneSlider.cs
@@ -14,22 +14,16 @@ public class InfiniteZoneSlider : MonoBehaviour
     private int zone = 19;
     private int spacing = 50;
     private bool isAnimating = false;
-    [SerializeField] private float shiftTime = .2f;
-
-    public void Start()
-    {
-        //ShiftLeft();
-    }
+    [SerializeField] private float shiftTime = .4f;
 
     public void ShiftLeft(Action EndAction = null)
     {
-        if (isAnimating) return; // Animasyon sürerken yeni kayma engellenir
+        if (isAnimating) return;
         isAnimating = true;
 
         float shiftAmount = itemWidth + spacing;
         int totalItems = items.Count;
 
-        // 1) Tüm item’ları DOTween ile sola kaydır
         int completedTweens = 0;
 
         foreach (var item in items)
@@ -40,7 +34,6 @@ public class InfiniteZoneSlider : MonoBehaviour
                 {
                     completedTweens++;
 
-                    // Son animasyon da bittiğinde
                     if (completedTweens == totalItems)
                     {
                         ReorderItems(shiftAmount);
@@ -49,21 +42,17 @@ public class InfiniteZoneSlider : MonoBehaviour
                     }
                 });
         }
-
     }
 
     private void ReorderItems(float shiftAmount)
     {
         RectTransform first = items[0];
 
-        // Eğer ilk item sol sınırı geçtiyse
         if (first.anchoredPosition.x <= leftBorderX)
         {
-            // Baştan çıkar, sona ekle
             items.RemoveAt(0);
             items.Add(first);
 
-            // Yeni pozisyon: en sağdaki item’ın sağı
             RectTransform last = items[items.Count - 2];
 
             first.anchoredPosition = new Vector2(
@@ -71,7 +60,6 @@ public class InfiniteZoneSlider : MonoBehaviour
                 first.anchoredPosition.y
             );
 
-            // Zone numarası artır
             zone++;
             first.GetComponent<TMPro.TextMeshProUGUI>().text = zone.ToString();
         }
