8774cce2f86945b867c3149dac432d9ff67c66e9
GorkemUner <gorkemuner00@gmail.com>
2025-11-19 09:52:39 +0300

    Feat: Remove unused RandomRangeExcept method from MathUtil
    Deleted the RandomRangeExcept method as it is no longer needed. This helps clean up the MathUtil utility class and reduces code maintenance overhead.

--- Assets/Scripts/Utils/MathUtil.cs
+++ Assets/Scripts/Utils/MathUtil.cs
@@ -4,22 +4,6 @@ using UnityEngine;
 
 public static class MathUtil
 {
-    public static int RandomRangeExcept(int minInclusive, int maxExclusive, List<int> except = null)
-    {
-        IEnumerable<int> allowedRandomNumbers = Enumerable.Range(minInclusive, maxExclusive - minInclusive);
-
-        if (except != null)
-            allowedRandomNumbers = allowedRandomNumbers.Except(except);
-
-        if (allowedRandomNumbers.Count() == 0)
-        {
-            Debug.LogError("Allowed numbers array is empty! Returning 0");
-            return 0;
-        }
-        int randomIndex = UnityEngine.Random.Range(0, allowedRandomNumbers.Count());
-        return allowedRandomNumbers.ElementAt(randomIndex);
-    }
-
     //Fisher-Yates Shuffle
     public static void Shuffle<T>(List<T> list)
     {
